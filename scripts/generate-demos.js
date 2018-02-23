70d71cc68ef95701df0864619ce14375d50ac53e
jrwebdev <web@jravenscroft.co.uk>
2018-02-24 12:07:49 +1300

    Add random colour + fix file formatting

--- scripts/generate-demos.js
+++ scripts/generate-demos.js
@@ -1,32 +1,38 @@
 const fs = require('fs-extra');
-const randomColor = require('random-color');
+const randomColor = require('randomcolor');
+const padStart = require('lodash/padStart');
+const range = require('lodash/range');
+const dedent = require('dedent');
+
+const componentName = index => `Component${padStart(index + 1, 4, '0')}`;
 
 const generateContents = number => {
   const color = randomColor();
-  return `import * as React from 'react';
-  import { storiesOf } from '@storybook/react';
-  
-  storiesOf('Component${number}', module)
-    .add('Example 1', () => (
-      <div style={{ backgroundColor: '${color}' }}>Example 1</div>
-    ))
-    .add('Example 2', () => (
-      <div style={{ backgroundColor: '${color}' }}>Example 2</div>
-    ))
-    .add('Example 3', () => (
-      <div style={{ backgroundColor: '${color}' }}>Example 3</div>
-    ))
-    .add('Example 4', () => (
-      <div style={{ backgroundColor: '${color}' }}>Example 4</div>
-    ))
-    .add('Example 5', () => (
-      <div style={{ backgroundColor: '${color}' }}>Example 5</div>
-    ))
+  return dedent`
+    import * as React from 'react';
+    import { storiesOf } from '@storybook/react';
+    
+    storiesOf('Component${number}', module)
+      .add('Example 1', () => (
+        <div style={{ backgroundColor: '${color}' }}>Example 1</div>
+      ))
+      .add('Example 2', () => (
+        <div style={{ backgroundColor: '${color}' }}>Example 2</div>
+      ))
+      .add('Example 3', () => (
+        <div style={{ backgroundColor: '${color}' }}>Example 3</div>
+      ))
+      .add('Example 4', () => (
+        <div style={{ backgroundColor: '${color}' }}>Example 4</div>
+      ))
+      .add('Example 5', () => (
+        <div style={{ backgroundColor: '${color}' }}>Example 5</div>
+      ))
   `;
 };
 
-Array.from(new Array(1000))
+range(1000)
   .map((val, index) => generateContents(index + 1))
   .forEach((contents, index) =>
-    fs.writeFile(`src/Component${index + 1}.demo.js`, contents)
+    fs.writeFile(`src/${componentName(index)}.demo.js`, contents)
   );
